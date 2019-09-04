791717431930d20a85814299b881b8da77c058eb
Alex <agordon1123@gmail.com>
2019-09-04 16:41:38 -0400

    GET requests working properly

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,13 +1,31 @@
-const express = 'express';
+const express = require('express');
+const db = require('./postDb');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    db.get()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    db.getById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ error: 'The user with the desired ID does not exist' })
+            };
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 router.delete('/:id', (req, res) => {
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,5 @@
-const express = 'express';
+const express = require('express');
+const db = require('./userDb');
 
 const router = express.Router();
 
@@ -11,15 +12,43 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-
+    db.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 router.get('/:id', (req, res) => {
-
+    const { id } = req.params;
+    db.getById(id) 
+        .then(user => {
+            if (user) {
+                res.status(200).json(user);
+            } else {
+                res.status(404).json({ error: 'The user with the desired ID does not exist' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 router.get('/:id/posts', (req, res) => {
-
+    const { id } = req.params;
+    db.getUserPosts(id)
+        .then(posts => {
+            if (posts) {
+                res.status(200).json(posts);
+            } else {
+                res.status(404).json({ error: 'The user with the desired ID does not exist' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'Internal server error' });
+        });
 });
 
 router.delete('/:id', (req, res) => {
